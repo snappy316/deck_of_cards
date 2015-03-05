683047ff22ec6e1389cd6d60ec26bb71891e2a09
David Ker <snappy316@gmail.com>
2015-03-05 08:51:33 -0800

    Closes #2, #3, #4, #5, #6, #7, #8

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -3,8 +3,8 @@ require 'card'
 
 describe "A card" do
   it "should have a suit and rank" do
-    QueenOfHearts = Card.new('12', 'Hearts')
-    QueenOfHearts.suit.must_equal('Hearts')
-    QueenOfHearts.rank.must_equal('12')
+    queen_of_hearts = Card.new('12', 'Hearts')
+    queen_of_hearts.suit.must_equal('Hearts')
+    queen_of_hearts.rank.must_equal('12')
   end
 end
